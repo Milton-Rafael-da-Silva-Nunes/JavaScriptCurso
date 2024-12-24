dae9b5cdda2972bea43d50e7c213d41a5ffde623
rafael_nunes <rafa.nuns17@gmail.com>
2024-12-24 16:31:01 -0300

    Trabalhando com Objetos - arrays

---
+++ script5.js
@@ -0,0 +1,17 @@
+// Objetos
+
+// let pessoa = {nome: "Rafael Nunes", idade: 28, altura: 1.71, cargo: "DBA"};
+
+// let carro = {nome: "Golf 1.6", cor: "Branco", ano: "2013"};
+
+// console.log(pessoa);
+// console.log(carro);
+
+let usuarios = [
+    {nome: "Rafael", cargo: "DBA", status: "Ativo"},
+    {nome: "Milton", cargo: "Programador", status: "Ativo"},
+    {nome: "José", cargo: "RH", status: "Ativo"}
+];
+
+console.log(usuarios); // Mostra o array
+console.log(usuarios[1].nome); // mostra somente o que eu filtrei
